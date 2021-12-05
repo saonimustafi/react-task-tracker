766a13ec501d1a8937d7e95b36ad3a4127301874
Saoni Mustafi <saonimustafi@Aveeks-MacBook-Pro.local>
2021-12-04 19:37:29 -0800

    Add inline styling, prop types and default props in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+
+const Header = (props) => {
     return (
         <header>
-            <h1>Task Tracker</h1>
+            <h1 style={headingStyle}>{props.title}</h1>
         </header>
     )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+
+// Styling in CSS
+const headingStyle = {
+    color: 'blue',
+    backgroundColor: 'Grey',
 }
 
 export default Header
